f411023b741b613e2a6ae531f6bd0d0b8e058544
Cristian Casali <casali.cristian01@gmail.com>
2022-07-25 11:51:36 +0200

    cvPalestra

--- app/src/main/java/com/example/applicazione/MainActivity.java
+++ app/src/main/java/com/example/applicazione/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.applicazione;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private CardView cvPalestra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        cvPalestra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Cliccato", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initView(){
+        cvPalestra = findViewById(R.id.cvPalestra);
     }
 }
 
