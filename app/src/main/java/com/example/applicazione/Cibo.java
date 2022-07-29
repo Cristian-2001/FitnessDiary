1879fef21e893aec7d06770f49caa690d564b4e8
Cristian Casali <casali.cristian01@gmail.com>
2022-07-29 13:49:46 +0200

    Classe Cibo

---
+++ app/src/main/java/com/example/applicazione/Cibo.java
@@ -0,0 +1,16 @@
+package com.example.applicazione;
+
+public class Cibo {
+    int id;
+    String nome;
+    String categoria;
+    double energia;
+    double lipidi;
+    double acidigrassi;
+    double colesterolo;
+    double carboidrati;
+    double zuccheri;
+    double fibre;
+    double proteine;
+    double sale;
+}
